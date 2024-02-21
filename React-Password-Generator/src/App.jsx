21e6dfc7adf60c36ea2f5e5dea4e31b01ccc3401
Kaif Kazi <Kaifkazi033@gmail.com>
2024-02-21 19:38:56 +0530

    Initialize React Password Generator Project

---
+++ React-Password-Generator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="w-full h-screen bg-slate-800"></div>
+    </>
+  );
+}
+
+export default App;
